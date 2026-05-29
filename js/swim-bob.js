de70c5b08b90db6c96c2437028ad3c6c1bef6008
Anna Baidikova <annabaidikova92@gmail.com>
2026-05-29 10:12:08 +0200

    fix borders and rocks

--- js/swim-bob.js
+++ js/swim-bob.js
@@ -7,14 +7,15 @@ function registerSwimBob() {
     },
 
     init: function () {
-      this.startY = this.el.object3D.position.y;
       this.time = 0;
+      this.prevOffset = 0;
     },
 
     tick: function (time, delta) {
       this.time += delta / 1000;
       const offset = Math.sin(this.time * Math.PI * 2 * this.data.frequency) * this.data.amplitude;
-      this.el.object3D.position.y = this.startY + offset;
+      this.el.object3D.position.y += offset - this.prevOffset;
+      this.prevOffset = offset;
     }
   });
 }
